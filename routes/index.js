5cf6e72183b9fdb7f89c7de24f78dbde268db75d
camilamoyac <cami.csmc@gmail.com>
2025-07-19 13:16:31 -0400

    connected db and get requests working

--- routes/index.js
+++ routes/index.js
@@ -2,4 +2,6 @@ const router = require("express").Router();
 
 router.get("/", (req, res) => {res.send("welcome to my world")});
 
+router.use("/contacts", require("./contacts"));
+
 module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require("express");
+
+const mongodb = require("./data/database.js");
 const app = express();
 
 const port = process.env.PORT || 3000;
 
 app.use("/", require("./routes"));
 
-app.listen(port, () => {console.log(`running on port ${port}`)})
+mongodb.initDb((err) => {
+    if (err){
+        console.log(err);
+    }
+    else{
+        app.listen(port, () => {console.log(`Database listening and node running on port ${port}`)})
+    }
+});
+
